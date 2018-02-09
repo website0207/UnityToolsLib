5b0d5a49cebadf30c82d7292950a1752711e993c
website0207 <dreamworks0207@163.com>
2018-02-09 17:28:00 +0800

    add object pool for GameObject and UI. And that, add singleton mode for GameObject or class

---
+++ ObjectPoolerScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolerScript
+{
+    //public static ObjectPoolerScript current;
+    public GameObject pooledObject;
+    public int pooledAmount = 20;
+    //用来判定该池是否能拓展
+    public bool willGrow = true;
+    public List<GameObject> pooledObjects;
+    /// <summary>
+    /// 对象池构造函数
+    /// </summary>
+    /// <param name="pooledObject">要装入池中的对象</param>
+    /// <param name="pooledAmount">池子默认大小</param>
+    /// <param name="willGrow">池子能否拓展</param>
+    public ObjectPoolerScript(GameObject pooledObject, int pooledAmount = 20, bool willGrow = true)
+    {
+        this.pooledObject = pooledObject;
+        this.pooledAmount = pooledAmount;
+        this.willGrow = willGrow;
+
+        pooledObjects = new List<GameObject>();
+        for (int i = 0; i < pooledAmount; i++)
+        {
+            GameObject obj = GameObject.Instantiate(pooledObject);
+            obj.SetActive(false);
+            pooledObjects.Add(obj);
+        }
+    }
+
+    //private void Awake()
+    //{
+        //current = this;
+    //}
+    // Use this for initialization
+    //void Start () {
+	//}
+
+    public GameObject GetPooledObject()
+    {
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            //不要对list进行remove操作，而用遍历的方法，因为遍历更快
+            if (!pooledObjects[i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        }
+        if (willGrow)
+        {
+            GameObject obj = GameObject.Instantiate(pooledObject);
+            pooledObjects.Add(obj);
+            return obj;
+        }
+        return null;
+    }
+}
---
+++ SingletonBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+//单例模式的实现
+public class SingletonBehaviour<T> : MonoBehaviour where T : Component
+{
+    protected static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                //如果这个组件存在，找到它
+                _instance = FindObjectOfType<T>();
+                if (_instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    _instance = obj.AddComponent<T>();
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    /// <summary>
+    /// Awake只调用一次，在脚本实例加载时执行
+    /// </summary>
+    public virtual void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        name = "_" + typeof(T).Name;
+        //可能会有问题，会和跨场景有关？
+        //if (_instance == null)
+        //{
+        //    _instance = this as T;
+        //}
+        //else
+        //{
+        //    Destroy(gameObject);
+        //}
+    }
+}
